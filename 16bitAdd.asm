48ad9bc1fee69f4a61923324c777747fc94bde62
Saumya-88031 <81921927+Saumya-88031@users.noreply.github.com>
2022-11-19 15:29:40 +0530

    Create 16bitAdd.asm

---
+++ 16bitAdd.asm
@@ -0,0 +1,14 @@
+; Addition of two 16 bit no. using 16 bit registers
+
+.MODEL SMALL
+.DATA
+AA DW 1234H
+BB DW 0BCDEH
+.CODE
+.STARTUP
+MOV BX,WORD PTR AA
+MOV CX,WORD PTR BB
+ADD BX,CX
+MOV AX,BX
+.EXIT
+END
